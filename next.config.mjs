7225e82a7999ba69397989aeb6f60cb4faaf5e62
Abdurrahman-Hassan <sufyanhassan123321@gmail.com>
2024-01-29 16:11:15 +0500

    remove pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-import withPWAInit from "@ducanh2912/next-pwa";
+/* import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-});
+}); */
 
-const nextConfig = withPWA({
+const nextConfig = {
   reactStrictMode: true,
-});
+};
 
 export default nextConfig;
